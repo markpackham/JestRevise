c80caac1b1a150e8be443307c03cee61a4f3a813
mark <markpackham1@gmail.com>
2024-01-25 15:32:10 +0000

    Finish off callback test demo
    https://youtu.be/IPiUDhwnZxA?si=7YaXcKgc7W8-Qj5_&t=2438

--- callbackDemo.test.js
+++ callbackDemo.test.js
@@ -1,1 +1,17 @@
 const fetchData = require('./callbackDemo')
+
+// Using done() to deal with callback
+// fetchData() uses a setTimeout so the answer to the test won't be instant
+test('The data is Hello World', done =>{
+    function callback(data){
+        try {
+            expect(data).toBe("Hello World")
+            // Remember to use done()
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    fetchData(callback);
+})
