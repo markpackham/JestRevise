175ae5c88d7df89257b1ff5df2abdab0c45e77ce
mark <markpackham1@gmail.com>
2024-01-26 17:44:54 +0000

    Demo Async Await version of Promise
    https://youtu.be/IPiUDhwnZxA?si=TuYat4CDg01y70Td&t=2903

--- promiseDemo.test.js
+++ promiseDemo.test.js
@@ -1,9 +1,16 @@
 const {fetchPromise, fetchPromiseReject} = require('./promiseDemo');
 
+// Normal Promises
 test('Data from Promise is "Hello World"', ()=>{
     return expect(fetchPromise()).resolves.toBe('Hello World');
 });
 
 test('The fetch fails with an error', ()=>{
     return expect(fetchPromiseReject()).rejects.toEqual('error');
+});
+
+// Use of Async Await
+test('Async Await test for "Hello World"', async ()=>{
+    const data = await fetchPromise();
+    expect(data).toBe('Hello World');
 });
