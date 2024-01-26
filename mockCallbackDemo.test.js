f036de9b314ffaebca97d793e097b7f9daa60402
mark <markpackham1@gmail.com>
2024-01-26 18:06:06 +0000

    Check mock function called with correct argument
    https://youtu.be/IPiUDhwnZxA?si=hXQUBzcRUs75ZGjK&t=3354

--- mockCallbackDemo.test.js
+++ mockCallbackDemo.test.js
@@ -2,5 +2,11 @@ test('Mock implementation of a basic function', ()=>{
     const  mock = jest.fn(x => 42 + x);
     // 42 + 0 aka "x" = 42
     expect(mock(0)).toBe(42);
+});
+
+test('Check mock argument called with correct argument', ()=>{
+    const  mock = jest.fn(x => 42 + x);   
     expect(mock(1)).toBe(43);
+    // Check mock argument called with correct argument of 1
+    expect(mock).toHaveBeenCalledWith(1);
 });
